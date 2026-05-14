eb4723d00c0873f78ed77b366737cb2c74cb88a3
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-05-14 12:40:38 +0000

    creature token component

--- src/client/map/gameMap.tsx
+++ src/client/map/gameMap.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import * as KV from 'react-konva';
 import './gameMap.css';
 import roomProvider from '../roomProvider';
+import CreatureToken from './creatureToken';
+import { CreatureState } from '../../common/gameState';
 
 export default function GameMap({ isGameMaster }: { isGameMaster?: boolean }): React.JSX.Element {
     const { room } = roomProvider.useRoom();
@@ -29,15 +31,7 @@ export default function GameMap({ isGameMaster }: { isGameMaster?: boolean }): R
         <KV.Stage width={size.width} height={size.height} className="game-map" >
             <KV.Layer>
                 {state.creatures.map((creature, idx) =>
-                    <KV.Circle
-                        key={idx}
-                        x={creature.position.x}
-                        y={creature.position.y}
-                        radius={50}
-                        draggable={isGameMaster}
-                        onDragMove={({ target }) => room.send("moveCreature", { idx, x: target.attrs.x, y: target.attrs.y })}
-                        fill="red"
-                    />
+                    <CreatureToken creature={creature as CreatureState} idx={idx} key={idx} draggable={isGameMaster} />
                 )}
             </KV.Layer>
         </KV.Stage>
