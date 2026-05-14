29e1224b5488f7b5f4442dbd1ab32cc1a0aa4ae1
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-05-14 13:07:35 +0000

    added use-image

--- src/client/map/creatureToken.tsx
+++ src/client/map/creatureToken.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Konva from 'konva';
 import * as KV from 'react-konva';
 import { CreatureState } from '../../common/gameState';
 import roomProvider from '../roomProvider';
@@ -6,18 +7,19 @@ import roomProvider from '../roomProvider';
 export default function CreatureToken({ creature, idx, draggable }: { creature: CreatureState, idx: number, draggable: boolean }): React.JSX.Element {
     const { room } = roomProvider.useRoom();
     return <>
-    <KV.Circle
-        x={creature.position.x}
-        y={creature.position.y}
-        radius={50}
-        draggable={draggable}
-        onDragMove={({ target }) => room.send("moveCreature", { idx, x: target.attrs.x, y: target.attrs.y })}
-        fill="red"
-    />
-    <KV.Text
-        x={creature.position.x}
-        y={creature.position.y}
-        text={creature.name}
-    />
+        <KV.Circle
+            x={creature.position.x}
+            y={creature.position.y}
+            draggable={draggable}
+            radius={50}
+            fill="red"
+            filters={[Konva.Filters.Emboss]}
+            onDragMove={({ target }) => room.send("moveCreature", { idx, x: target.attrs.x, y: target.attrs.y })}
+        />
+        <KV.Text
+            x={creature.position.x}
+            y={creature.position.y}
+            text={creature.name}
+        />
     </>;
 }
