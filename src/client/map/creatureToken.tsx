fee115f3225ba68183be211d051d5a7ca9609691
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-05-19 14:06:33 +0000

    less jittery GM creature dragging

--- src/client/map/creatureToken.tsx
+++ src/client/map/creatureToken.tsx
@@ -6,19 +6,31 @@ import roomProvider from '../roomProvider';
 
 export default function CreatureToken({ creature, idx, draggable }: { creature: CreatureState, idx: number, draggable: boolean }): React.JSX.Element {
     const { room } = roomProvider.useRoom();
+    const [dragging, setDragging] = React.useState(false);
+    const [position, setPosition] = React.useState<{ x: number, y: number }>({ x: creature.position.x, y: creature.position.y });
+
+    React.useEffect(() => {
+        if (!dragging) setPosition(creature.position);
+    }, [creature]);
+
     return <>
         <KV.Circle
-            x={creature.position.x}
-            y={creature.position.y}
+            x={position.x}
+            y={position.y}
             draggable={draggable}
             radius={50}
             fill="red"
             filters={[Konva.Filters.Emboss]}
-            onDragMove={({ target }) => room.send("moveCreature", { idx, x: target.attrs.x, y: target.attrs.y })}
+            onDragStart={() => setDragging(true)}
+            onDragMove={({ target }) => {
+                setPosition({ x: target.attrs.x, y: target.attrs.y });
+                room.send("moveCreature", { idx, x: target.attrs.x, y: target.attrs.y });
+            }}
+            onDragEnd={() => setDragging(false)}
         />
         <KV.Text
-            x={creature.position.x}
-            y={creature.position.y}
+            x={position.x}
+            y={position.y}
             text={creature.name}
         />
     </>;
