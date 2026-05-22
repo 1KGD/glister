711db0ad988dddb1b3adb8c988c27e5288eefc4d
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-05-22 13:20:17 +0000

    oops, trigger room dispose timer on GM leave

--- src/server/gameRoom.ts
+++ src/server/gameRoom.ts
@@ -58,15 +58,14 @@ export default class GameRoom extends Colyseus.Room<{
     }
 
     public override onLeave(client: Client): void {
+        if (this.clients.length === 0) {
+            this.disposeTimer = this.clock.setTimeout(() => this.disconnect(), config.multiplayer.roomTimeout);
+        }
         if (this.state.gameMaster?.id === client.sessionId) {
             this.state.gameMaster = null;
             return;
         };
         this.state.creatures.delete(this.state.players.get(client.sessionId).creatureId);
         this.state.players.delete(client.sessionId);
-
-        if (this.clients.length === 0) {
-            this.disposeTimer = this.clock.setTimeout(() => this.disconnect(), config.multiplayer.roomTimeout);
-        }
     }
 }
