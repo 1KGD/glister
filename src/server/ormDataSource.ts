afa64d374a4dcfbd05366644b5f31745cdf56b27
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-06-08 13:11:57 +0000

    re-enable SQL logging in dev enviorment

--- src/server/ormDataSource.ts
+++ src/server/ormDataSource.ts
@@ -8,6 +8,6 @@ export default new ORM.DataSource({
     type: "better-sqlite3",
     database: "./data/server.db3",
     synchronize: true,
-    logging: false,//config.dev,
+    logging: config.dev,
     entities: [Account, SessionToken, CelestialSystem, Ship]
 });
