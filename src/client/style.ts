1ed2bd38cbb499206480c59def5f84e487c8992c
mrmcscreamypant <161328176+mrmcscreamypant@users.noreply.github.com>
2026-06-22 12:07:24 +0000

    More things

--- src/client/style.ts
+++ src/client/style.ts
@@ -4,9 +4,9 @@ export const theme = {
     space: Arwes.createThemeUnit((index) => `${index * 0.25}rem`),
     spacen: Arwes.createThemeMultiplier((index) => index * 4),
     colors: {
-        background: 'hsla(90, 100%, 3%)',
-        primary: Arwes.createThemeColor((i) => [0, 100, 100 - i * 10]),
-        secondary: Arwes.createThemeColor((i) => [30, 100, 100 - i * 10])
+        background: 'hsla(255, 100%, 3%)',
+        primary: Arwes.createThemeColor((i) => [255, 100, 100 - i * 10]),
+        secondary: Arwes.createThemeColor((i) => [270, 100, 100 - i * 10])
     }
 };
 
@@ -19,11 +19,11 @@ export default {
     "--primary-color-4": theme.colors.primary(4),
     "--primary-color-5": theme.colors.primary(5),
 
-    "--primary-color-1-transparent": theme.colors.primary(1, {alpha: 0.05}),
-    "--primary-color-2-transparent": theme.colors.primary(2, {alpha: 0.05}),
-    "--primary-color-3-transparent": theme.colors.primary(3, {alpha: 0.05}),
-    "--primary-color-4-transparent": theme.colors.primary(4, {alpha: 0.05}),
-    "--primary-color-5-transparent": theme.colors.primary(5, {alpha: 0.05}),
+    "--primary-color-1-transparent": theme.colors.primary(1, { alpha: 0.05 }),
+    "--primary-color-2-transparent": theme.colors.primary(2, { alpha: 0.05 }),
+    "--primary-color-3-transparent": theme.colors.primary(3, { alpha: 0.05 }),
+    "--primary-color-4-transparent": theme.colors.primary(4, { alpha: 0.05 }),
+    "--primary-color-5-transparent": theme.colors.primary(5, { alpha: 0.05 }),
 
     "--secondary-color-1": theme.colors.secondary(1),
     "--secondary-color-2": theme.colors.secondary(2),
@@ -31,9 +31,9 @@ export default {
     "--secondary-color-4": theme.colors.secondary(4),
     "--secondary-color-5": theme.colors.secondary(5),
 
-    "--secondary-color-1-transparent": theme.colors.secondary(1, {alpha: 0.05}),
-    "--secondary-color-2-transparent": theme.colors.secondary(2, {alpha: 0.05}),
-    "--secondary-color-3-transparent": theme.colors.secondary(3, {alpha: 0.05}),
-    "--secondary-color-4-transparent": theme.colors.secondary(4, {alpha: 0.05}),
-    "--secondary-color-5-transparent": theme.colors.secondary(5, {alpha: 0.05}),
+    "--secondary-color-1-transparent": theme.colors.secondary(1, { alpha: 0.05 }),
+    "--secondary-color-2-transparent": theme.colors.secondary(2, { alpha: 0.05 }),
+    "--secondary-color-3-transparent": theme.colors.secondary(3, { alpha: 0.05 }),
+    "--secondary-color-4-transparent": theme.colors.secondary(4, { alpha: 0.05 }),
+    "--secondary-color-5-transparent": theme.colors.secondary(5, { alpha: 0.05 }),
 };
